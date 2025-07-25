cd27867054e8b25aaa957b6750f7965155f0ccf2
Sammy-Koch <samkoch@umich.edu>
2025-07-24 21:49:28 -0400

    Add code for finished working product
    The program works as intended
    however, could add game nuance allowing player more choice later on

--- farkle.cpp
+++ farkle.cpp
@@ -1,1 +1,7 @@
 #include "farkle.h"
+
+int main() {
+  Game game;
+  game.getPlayers();
+  game.play();
+}
--- farkle.h
+++ farkle.h
@@ -20,11 +20,8 @@ class Dice {
       return value;
     }
 
-    void roll() {
-      unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-      std::default_random_engine generator(seed);
-      std::uniform_int_distribution<int> distribution(1, 6);
-      value = distribution(generator);
+    void roll(std::default_random_engine& gen, std::uniform_int_distribution<int>& dist) {
+      value = dist(gen);
     }
 
   private:
@@ -34,10 +31,22 @@ class Dice {
 
 class DiceShaker {
   public:
-  DiceShaker() : shaker(6) {}
+  DiceShaker() : shaker(6) {
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    generator.seed(seed); // Initialize the member generator
+  }
 
   int getSize() {
     return 6;
+  }
+
+  bool checkIfEmpty() {
+    for(auto &d : shaker) {
+      if(d.getInShaker()) {
+        return false;
+      }
+    }
+    return true;
   }
 
   void resetShaker() {
@@ -46,18 +55,33 @@ class DiceShaker {
         for (int i = 0; i < 6; ++i) {
             shaker.push_back(Dice()); // Add 6 new, default-constructed Dice (inShaker=true)
         }
-    }
+  }
+
+  void updateShaker() {
+    shaker.erase(std::remove_if(shaker.begin(), shaker.end(),
+                          [](Dice& d) {
+                              return !d.getInShaker(); // Remove if NOT in shaker
+                          }),
+            shaker.end());
+  }
 
   std::vector<Dice> &shakeAndRoll() {
-    for(int i = 0; i < shaker.size(); i++) {
+    std::cout << "\nRoll: \n";
+    for(size_t i = 0; i < shaker.size(); i++) {
       if(shaker[i].getInShaker()) {
-        shaker[i].roll();
-        // TODO: print value of die
+        shaker[i].roll(generator, distribution);
+        // print value of die
+        std::cout << shaker[i].getValue() << " ";
       }
     }
+    std::cout << "\n\n";
+    return shaker;
   }
   private:
   std::vector<Dice> shaker;
+
+  std::default_random_engine generator;
+  std::uniform_int_distribution<int> distribution{1, 6};
 };
 
 class Game {
@@ -68,33 +92,36 @@ class Game {
     std::string name;
   };
 
-  std::vector<Dice> rollDice() {
-    diceShaker.shakeAndRoll();
+  std::vector<Dice> &rollDice() {
+    if(diceShaker.checkIfEmpty()) {
+      diceShaker.resetShaker();
+    }
+    return diceShaker.shakeAndRoll();
   }
 
   int select(std::vector<Dice> &shake) {
     int pointTally = 0;
     // of a kinds
-    std::unordered_map<int, std::vector<Dice>> diceGroups;
+    std::unordered_map<int, std::vector<Dice*>> diceGroups;
     for(auto &die : shake) {
-      diceGroups[die.getValue()].push_back(die);
+      diceGroups[die.getValue()].push_back(&die);
     }
     
     for(auto &p : diceGroups) {
       if(p.second.size() == 6) { // 6 of a kind
         pointTally += 3000;
         for(auto &d : p.second) {
-          d.setInShaker(false);
+          d->setInShaker(false);
         }
       } else if(p.second.size() == 5) { // 5 of a kind
         pointTally += 2000;
         for(auto &d : p.second) {
-          d.setInShaker(false);
+          d->setInShaker(false);
         }
       } else if(p.second.size() == 4) { // 4 of a kind AND 4 + pair
         pointTally += 1000;
         for(auto &d : p.second) {
-          d.setInShaker(false);
+          d->setInShaker(false);
         }
         if(diceGroups.size() == 2 && shake.size() == 6) {
           pointTally += 500;
@@ -131,53 +158,50 @@ class Game {
       if(p.second.size() == 3) {
         bool overlap = false;
         for(auto &d : p.second) {
-          if(!d.getInShaker()) {
+          if(!d->getInShaker()) {
             overlap = true;
           }
         }
         if(!overlap) {
           pointTally += p.first * 100;
           for(auto &d : p.second) {
-            d.setInShaker(false);
+            d->setInShaker(false);
           }
         }
       }
     }
     // 1s and 5s
-    // TODO make sure no overlap with previous
+    // make sure no overlap with previous
     if(diceGroups[1].size() != 0) {
       for(auto &d : diceGroups[1]) {
-        if(d.getInShaker()) {
+        if(d->getInShaker()) {
           pointTally += 100;
         }
       }
     }
     if(diceGroups[5].size() != 0) {
       for(auto &d : diceGroups[5]) {
-        if(d.getInShaker()) {
+        if(d->getInShaker()) {
           pointTally += 50;
         }
       }
     }
 
     for(auto &d : diceGroups[1]) {
-      d.setInShaker(false);
+      d->setInShaker(false);
     }
     for(auto &d : diceGroups[5]) {
-      d.setInShaker(false);
+      d->setInShaker(false);
     }
 
     // update shaker
-    shake.erase(std::remove_if(shake.begin(), shake.end(),
-                          [](Dice& d) {
-                              return !d.getInShaker(); // Remove if NOT in shaker
-                          }),
-            shake.end());
+    diceShaker.updateShaker();
     
     return pointTally;
   }
 
   void turn(Player &player) {
+    std::cout << player.name << "\'s turn!\n";
     // does one turn for a single player
     // roll until no available points or player banks
     // make sure to reset shaker as needed
@@ -191,30 +215,66 @@ class Game {
     // if all 6 dice rolled and selected, then reset shaker and repeat
     //   make sure to offer banking after 6 as well.
     bool rollAgain = true;
-    std::vector<Dice> roll;
     int pointsTally = 0;
     while(rollAgain) {
-      roll = rollDice();
-      pointsTally += select(roll);
-      if(select(roll) == 0) {
+      std::vector<Dice> &roll = rollDice();
+      int selection = select(roll);
+      pointsTally += selection;
+      if(selection == 0) {
         // roll farkled
+        std::cout << "* * * * FARKLE! * * * *\n";
+        std::cout << "      FARKLE!\n";
+        std::cout << "* * * * FARKLE! * * * *\n\n";
+        std::cout << "Better luck next time!\n\n";
         pointsTally = 0;
         break;
       }
       char choice;
-      std::cout << "Would you like to roll again or bank? (R/B): ";
+      std::cout << selection << " points have been added to your tally (for now..)\n";
+      std::cout << "Your total for this turn so far is: " << pointsTally << "\n";
+      std::cout << "Would you like to roll again or bank those points? (R/B): ";
       std::cin >> choice;
       choice = std::tolower(choice);
-      while(choice != 'r' || choice != 'b') {
+      while(choice != 'r' && choice != 'b') {
         std::cout << "Please enter \"R\" or \"B\": ";
         std::cin >> choice;
         choice = std::tolower(choice);
       }
       if(choice == 'b') {
         rollAgain = false;
+        std::cout << "\nWell done " << player.name << " , " << pointsTally << " points have been added to your score!\n\n";
       }
     }
     player.points += pointsTally;
+    diceShaker.resetShaker();
+    std::cout << "--------------------\n";
+    std::cout << "CURRENT SCOREBOARD: \n";
+    for(auto &p : players) {
+      std::cout << p.name << ": " << p.points << "\n";
+    }
+    std::cout << "--------------------\n\n";
+  }
+
+  void getPlayers() {
+    std::cout << "Welcome to FARKLE!" << "\n" << std::endl;
+    std::string name = "q";
+    std::vector<std::string> playerNames;
+    while(name == "Q" || name == "q") {
+      std::cout << "Please enter the first player's name (name unfortunately can not be \"Q\"): ";
+      std::cin >> name;
+    }
+    playerNames.push_back(name);
+
+    while(name != "q" && name != "Q") {
+      std::cout << "Please enter a player name (enter \"Q\" if all players have been entered): ";
+      std::cin >> name;
+      playerNames.push_back(name);
+    }
+
+    for(size_t i = 0; i < playerNames.size() - 1; i++) {
+      Player p = {0, false, playerNames[i]};
+      players.push_back(p);
+    }
   }
 
   void play() {
@@ -230,6 +290,10 @@ class Game {
     }
 
     // TODO print scoreboard
+    std::cout << "FINAL SCOREBOARD:\n";
+    for(auto &p : players) {
+      std::cout << p.name << ": " << p.points << "\n";
+    }
   }
 
   private:
